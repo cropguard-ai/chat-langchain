6f3760804c246eaad033defd7ebca77987d4d68b
simdage <s.dagenais53@gmail.com>
2024-03-27 07:56:46 -0400

    removing files from return if duplicate SP

--- croptalk/document_retriever.py
+++ croptalk/document_retriever.py
@@ -79,7 +79,7 @@ class DocumentRetriever:
         return formatted_docs
 
     @staticmethod
-    def read_pdf_from_s3(bucket_name, file_key):
+    def _read_pdf_from_s3(bucket_name, file_key):
         # Initialize S3 client
         s3 = boto3.client('s3',
                           aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
@@ -97,6 +97,48 @@ class DocumentRetriever:
 
         return text_content
 
+    @staticmethod
+    def _clean_sp_content(content: str) -> str:
+        """
+        Removing multiple spaces and line skips to save on tokens
+        """
+        return ' '.join(content.replace("\n", " ").split())
+
+    @staticmethod
+    def _find_duplicates_indices(enum):
+        seen = {}
+        duplicates = []
+
+        for idx, item in enum:
+            if item in seen:
+                duplicates.append(idx)
+            else:
+                seen[item] = idx
+
+        return duplicates
+
+    def _get_index_from_unique_sp_s3_keys(self, query_response: QueryReturn) -> List[int]:
+        # getting list of duplicated s3 keys for doc which are SP
+        s3_keys = [(i, doc) for i, doc in enumerate(query_response.objects) if doc.properties["doc_category"] == "SP"]
+        duplicated_sp_idx = self._find_duplicates_indices(s3_keys)
+
+        # get all unique indexes without SP duplicates
+        return [i for i, j in enumerate(query_response.objects) if i not in duplicated_sp_idx]
+
+    def _get_content(self, doc_property) -> str:
+        """
+        If the document is of type SP, we extract the full text of the document to be inserted in the prompt
+        Args:
+            doc_property: properties attribute from query response objects
+        Returns: formatted str
+        """
+        if doc_property["doc_category"] == "SP":
+            content = self._read_pdf_from_s3("croptalk-spoi", doc_property['s3_key'])
+            content = self._clean_sp_content(content)
+        else:
+            content = doc_property['content']
+        return f">{content}</doc>"
+
     def _format_query_response(self, query_response: QueryReturn) -> List[str]:
         """
         Args:
@@ -105,6 +147,8 @@ class DocumentRetriever:
         Returns:
             list of retrieved and formatted documents, equivalent to provided query response
         """
+
+        unique_index = self._get_index_from_unique_sp_s3_keys(query_response)
 
         response_objects = [
             f"<doc"
@@ -117,12 +161,8 @@ class DocumentRetriever:
             f" county='{doc.properties['county']}'"
             f" s3_key='{doc.properties['s3_key']}'"
             f" url='https://croptalk-spoi.s3.us-east-2.amazonaws.com/{doc.properties['s3_key']}'"
-            for i, doc in enumerate(query_response.objects)
+            + self._get_content(doc.properties)
+            for i, doc in enumerate(query_response.objects) if i in unique_index
         ]
 
-        for i, doc in enumerate(query_response.objects):
-            if doc.properties["doc_category"] == "SP":
-                response_objects[i] += self.read_pdf_from_s3("croptalk-spoi", doc.properties['s3_key'])
-            else:
-                response_objects[i] += f">{doc.properties['content']}</doc>"
         return response_objects
